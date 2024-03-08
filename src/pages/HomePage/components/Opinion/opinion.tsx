ec0e38200ac740e48afee3b064a23ecf16da84c5
Tarık Emir <trkygl1900@gmail.com>
2024-03-08 15:20:17 +0300

    opinion added

--- src/pages/HomePage/components/Opinion/opinion.tsx
+++ src/pages/HomePage/components/Opinion/opinion.tsx
@@ -1,6 +1,20 @@
 const opinion = () => {
   return (
-    <div>opinion</div>
+    <div className="m-auto shadow-2xl mb-4 mt-4 text-left">
+      <h3 className="mx-5 text-l font-bold pt-2">Maoni</h3>
+      <div className="px-4 py-8 font-bold">
+          <img src="https://picsum.photos/900/400?random=1" />
+          <p>Mabadiliko ya kimkakati: Jukumu la UAE lenye kubadilika katika kesi ya Libya</p>
+      </div>
+      <div className="px-4 py-8 font-bold">
+        <img src="https://picsum.photos/900/400?random=3" />
+        <p>Misri inajaribu kusaidia Wapalestina, sio kuwaweka kwenye uzio wa mpaka</p>
+      </div>
+      <div className="px-4 py-8 font-bold">
+        <img src="https://picsum.photos/900/400?random=2" />
+        <p>Udongo wa polima: Kuzama katika  usanii usio na kikomo</p>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    colors: {
-      footer: '#01000d',
-      footerh4: '#00b6ca ',
-      footerp:'#D1D9D9'
+    
+    extend: {
+      colors: {
+        footer: '#01000d',
+        footerh4: '#00b6ca ',
+        footerp:'#D1D9D9'
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
