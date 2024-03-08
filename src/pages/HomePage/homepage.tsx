fd4aeb9762f3eeb040a8e61138187981c31c123c
mikailafsin <mikailwnchstr@gmail.com>
2024-03-08 15:18:04 +0300

    articles eklendi.

--- src/pages/HomePage/homepage.tsx
+++ src/pages/HomePage/homepage.tsx
@@ -1,23 +1,21 @@
-import Articles from "./components/Articles/articles"
-import Headline from "./components/HeadLine/headline"
-import Latestfeatured from "./components/LatestFeatured/latestfeatured"
-import Morearticles from "./components/MoreArticles/morearticles"
-import Opinion from "./components/Opinion/opinion"
-import Videos from "./components/Videos/videos"
+import Articles from "./components/Articles/articles";
+import Headline from "./components/HeadLine/headline";
+import Latestfeatured from "./components/LatestFeatured/latestfeatured";
+import Morearticles from "./components/MoreArticles/morearticles";
+import Opinion from "./components/Opinion/opinion";
+import Videos from "./components/Videos/videos";
 
 const homepage = () => {
-    return (
-        <div className="min-h-screen" >
+  return (
+    <div className="min-h-screen">
+      <Articles />
+      <Headline />
+      <Latestfeatured />
+      <Morearticles />
+      <Opinion />
+      <Videos />
+    </div>
+  );
+};
 
-            <Articles/>
-            <Headline/>
-            <Latestfeatured/>
-            <Morearticles/>
-            <Opinion/>
-            <Videos/>
-
-        </div>
-    )
-}
-
-export default homepage
+export default homepage;
