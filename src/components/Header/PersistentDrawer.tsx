721054c317fb76ea47ee7326bc5b671d57231fef
Egemen Gunduz <86930714+gndzegemen@users.noreply.github.com>
2024-03-12 08:04:35 +0300

    fix: drawer width and color

--- src/components/Header/PersistentDrawer.tsx
+++ src/components/Header/PersistentDrawer.tsx
@@ -3,20 +3,11 @@ import React, {useState} from 'react';
 import {styled, useTheme} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import CloseIcon from '@mui/icons-material/Close';
 
 // Define the width of the drawer
@@ -46,7 +37,7 @@ function PersistentDrawer({pages}) {
 
     // Return JSX for PersistentDrawer component
     return (
-        <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none'}}}>
 
             {/* IconButton to open the drawer */}
             <IconButton
@@ -75,9 +66,12 @@ function PersistentDrawer({pages}) {
                 }}
                 open={Boolean(open)}
                 onClose={handleDrawerClose}
+
                 sx={{
-                    display: {xs: 'block', md: 'none',},
+                    display: {xs: 'block', md: 'none'},
+
                 }}
+
             >
                 {/* Drawer header with IconButton to close the drawer */}
                 <DrawerHeader>
@@ -87,7 +81,7 @@ function PersistentDrawer({pages}) {
                 </DrawerHeader>
 
                 {/* List items for the drawer */}
-                <List sx={{backgroundColor: "black"}}>
+                <List sx={  {flexGrow: 1,backgroundColor: "black", width:350 }}>
                     {/* Map over 'pages' prop to render list items */}
                     {pages.map((page) => (
                         <ListItemButton key={page} onClick={handleDrawerClose}>
